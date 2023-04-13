2270d293af85227d0f6d8998aaaaeda88ab62c14
aoikurokawa <aoi.kurokawa@nxted.co.jp>
2023-04-13 15:40:55 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+struct Compiler {
+    last_elm_end_line: u32,
+    decorate: Vec<String>,
+}
+
+impl Compiler {
+    fn new() -> Self {
+        Self {
+            last_elm_end_line: 1,
+            decorate: Vec::new(),
+        }
+    }
+
+    fn is_decorate_element() -> bool {
+        true
+    }
+
+    fn compile() -> String {
+        "".to_string()
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
